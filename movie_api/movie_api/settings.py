65253f8c0b4414683df82adb0f96d6396571dc4d
Evans Omia <evansomia005@gmail.com>
2026-01-02 15:17:27 +0400

    added the functionalities to edit and delete a review

--- movie_api/movie_api/settings.py
+++ movie_api/movie_api/settings.py
@@ -131,7 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+
 MEDIA_URL = 'media/'
 
 STATICFILES_DIR = ''
--- movie_api/review/views.py
+++ movie_api/review/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Review
 from .forms import ReviewForm
@@ -19,3 +19,25 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse_lazy("movie-detail", kwargs={"pk": self.kwargs["pk"]})
+    
+class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review_form.html"
+
+    def test_func(self):
+        return self.get_object().user == self.request.user
+
+    def get_success_url(self):
+        return reverse("movie-detail", kwargs={"pk": self.object.movie.pk})
+
+
+class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Review
+    template_name = "reviews/review_confirm_delete.html"
+
+    def test_func(self):
+        return self.get_object().user == self.request.user
+
+    def get_success_url(self):
+        return reverse("movie-detail", kwargs={"pk": self.object.movie.pk})
