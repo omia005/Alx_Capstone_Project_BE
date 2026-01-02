57658a945e1941218d7b9e3be7fae28cee6dc6dd
Evans Omia <evansomia005@gmail.com>
2026-01-02 17:18:02 +0400

    added the crud for reviews including also searching for reviews

--- movie_api/review/urls.py
+++ movie_api/review/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ReviewCreateView
+from .views import ReviewCreateView, ReviewDeleteView, ReviewUpdateView, ReviewSearchView
 
 urlpatterns = [
     path("add/<int:pk>/", ReviewCreateView.as_view(), name="review-add"),
+    path("<int:pk>/edit/", ReviewUpdateView.as_view(), name="review-edit"),
+    path("<int:pk>/delete/", ReviewDeleteView.as_view(), name="review-delete"),
+    path("search/", ReviewSearchView.as_view(), name="review-search"),
 ]
--- movie_api/review/views.py
+++ movie_api/review/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import Review
 from .forms import ReviewForm
 from movies.models import Movie
+from django.db.models import Q
 
 #create your views here
 class ReviewCreateView(LoginRequiredMixin, CreateView):
@@ -23,7 +24,7 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Review
     form_class = ReviewForm
-    template_name = "reviews/review_form.html"
+    template_name = "review/review_form.html"
 
     def test_func(self):
         return self.get_object().user == self.request.user
@@ -34,10 +35,31 @@ class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Review
-    template_name = "reviews/review_confirm_delete.html"
+    template_name = "review/confirm_delete.html"
 
     def test_func(self):
         return self.get_object().user == self.request.user
 
     def get_success_url(self):
         return reverse("movie-detail", kwargs={"pk": self.object.movie.pk})
+    
+class ReviewSearchView(ListView):
+    model = Review
+    template_name = "review/review_search.html"
+    context_object_name = "reviews"
+
+    def get_queryset(self):
+        queryset = Review.objects.select_related("movie", "user")
+
+        query = self.request.GET.get("q")
+        rating = self.request.GET.get("rating")
+
+        if query:
+            queryset = queryset.filter(
+                Q(movie__title__icontains=query)
+            )
+
+        if rating:
+            queryset = queryset.filter(rating=rating)
+
+        return queryset
